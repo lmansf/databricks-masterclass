7c82c04a34052728e70bbabaff70316879372bd5
Afaque Ahmad <linkedafaque@gmail.com>
2025-12-29 19:05:40 +0400

    chore: updated streaming script to send data to Eventhub

--- projects/databricks-e2e-project/synthetic_data/streaming_orders.py
+++ projects/databricks-e2e-project/synthetic_data/streaming_orders.py
@@ -1,3 +1,4 @@
+import os
 import json
 import random
 import time
@@ -5,17 +6,15 @@ from datetime import datetime
 from azure.eventhub import EventHubProducerClient, EventData
 import pandas as pd
 
-# Configuration
 from dotenv import load_dotenv
 load_dotenv()   
 
 EVENTHUB_CONNECTION_STRING = os.getenv("EVENTHUB_CONNECTION_STRING")
 EVENTHUB_NAME = os.getenv("EVENTHUB_NAME")
 
-import os
-script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Load data
+script_dir = os.path.dirname(os.path.abspath(__file__))
 df_restaurants = pd.read_csv(os.path.join(script_dir, "data", "restaurants.csv"))
 df_customers = pd.read_csv(os.path.join(script_dir, "data", "customers.csv"))
 df_menu_items = pd.read_csv(os.path.join(script_dir, "data", "menu_items.csv"))
@@ -72,20 +71,20 @@ def generate_order():
     }
 
 def stream_to_eventhub(interval_seconds=3, max_orders=None):
-    # producer = EventHubProducerClient.from_connection_string(
-    #     conn_str=EVENTHUB_CONNECTION_STRING,
-    #     eventhub_name=EVENTHUB_NAME
-    # )
+    producer = EventHubProducerClient.from_connection_string(
+        conn_str=EVENTHUB_CONNECTION_STRING,
+        eventhub_name=EVENTHUB_NAME
+    )
     
-    print(f"🚀 Streaming to Event Hub: {EVENTHUB_NAME}")
+    print(f"\n\nStreaming to Event Hub: {EVENTHUB_NAME}")
     order_count = 0
     
     try:
         while True:
             order = generate_order()
-            # event_data_batch = producer.create_batch()
-            # event_data_batch.add(EventData(json.dumps(order)))
-            # producer.send_batch(event_data_batch)
+            event_data_batch = producer.create_batch()
+            event_data_batch.add(EventData(json.dumps(order)))
+            producer.send_batch(event_data_batch)
             
             order_count += 1
             print()
@@ -101,7 +100,7 @@ def stream_to_eventhub(interval_seconds=3, max_orders=None):
     except KeyboardInterrupt:
         print("\nStopped")
     finally:
-        # producer.close()
+        producer.close()
         pass
 
 if __name__ == "__main__":
